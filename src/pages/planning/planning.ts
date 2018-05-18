ed85ac98b02b6c4800d0ccd61d9e0316abbf0ae3
Henrik Müller <henrikmueller@me.com>
2018-05-18 21:06:31 +0200

    add color + beautify protocol

--- src/pages/planning/planning.ts
+++ src/pages/planning/planning.ts
@@ -8,15 +8,27 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class PlanningPage {
 
-  public items = []; // Protokoll items
-  public drivers = []; // Fahrerplanung
-
+  public protocolItems: Array<{name: string, icon: string, timestamp: any, duration: any}>; // Protokoll items
+  public drivers: Array<{name: string, icon: string}>;
   public reorderIsEnabled = false; // Ordnen der Fahrerplanung Button
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
 
-  
+    this.protocolItems = [
+      { name: 'Johannes', icon: 'person', timestamp: '13:37', duration: '0:32'},
+      { name: 'Marcus', icon: 'person', timestamp: '14:24', duration: '0:53' },
+      { name: 'Alex', icon: 'person', timestamp: '15:34', duration: '2:12' },
+      { name: 'Marie', icon: 'person', timestamp: '3:15', duration: '1:16' }
+    ];
+
+    this.drivers = [
+      { name: 'Alice', icon: 'person' },
+      { name: 'Bob', icon: 'person' },
+      { name: 'James', icon: 'person' },
+      { name: 'Frank', icon: 'person' }
+    ];
+
+  }
 
   toggleReorder() {
     this.reorderIsEnabled = !this.reorderIsEnabled;
